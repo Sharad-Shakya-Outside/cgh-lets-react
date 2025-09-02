aed9736055c9ed43c4cc10b28a42b3a99e32bf68
Sharad Shakya <sharad.shakya@outside.studio>
2025-09-02 11:00:26 +0545

    Fix nav links having pointer events none

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,10 +1,10 @@
 export const Loader = () => {
   return (
     <>
-      <div class="flex justify-center items-center w-full py-[120px]">
-        <div class="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-purple-500"></div>
-        <div class="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-red-500 ml-3"></div>
-        <div class="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-blue-500 ml-3"></div>
+      <div className="flex justify-center items-center w-full py-[120px]">
+        <div className="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-purple-500"></div>
+        <div className="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-red-500 ml-3"></div>
+        <div className="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-blue-500 ml-3"></div>
       </div>
     </>
   );
